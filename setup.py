83112b53e465448267a092975e51ced0d067b8f7
Ted Kaemming <ted@kaemming.com>
2010-09-17 21:34:03 -0500

    bump version to 1.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-orderable',
-    version = '1.2',
+    version = '1.2.1',
     description = 'Model ordering for the Django administration site.',
     
     author = 'Ted Kaemming',
